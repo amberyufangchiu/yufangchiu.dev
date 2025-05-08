d1813687ac248de745710d996165b588d6fd52e3
Amber Chiu <suzumiya1235@gmail.com>
2025-05-08 13:47:12 +0800

    style: update Contributions component for consistent styling and add total count visibility option

--- src/app/components/Contributions.tsx
+++ src/app/components/Contributions.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
 
 interface contributionsProps {
   darkMode: boolean;
@@ -8,15 +8,16 @@ interface contributionsProps {
 const Contributions = ({ darkMode }: contributionsProps) => {
   return (
     <section className="mt-28">
-      <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-6 text-gray-900 dark:text-gray-100">
+      <h2 className="mb-6 text-center text-3xl font-bold tracking-tighter text-gray-900 sm:text-5xl dark:text-gray-100">
         Always Tinkering 👩🏻‍💻
       </h2>
 
       <GitHubCalendar
         username="amberyufangchiu"
-        colorScheme={darkMode ? "dark" : "light"}
+        colorScheme={darkMode ? 'dark' : 'light'}
         hideMonthLabels={true}
         hideColorLegend={true}
+        hideTotalCount={true}
       />
     </section>
   );
